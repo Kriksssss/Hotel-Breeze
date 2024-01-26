a54944b04495d9e4efb5902a95c119f3b4787128
Kriksssss <nabibeg53@gmail.com>
2024-01-26 21:51:49 +0300

    BreezeHotelApplication

---
+++ src/test/java/com/dailycodework/hotelbreeze/HotelBreezeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodework.hotelbreeze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBreezeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
