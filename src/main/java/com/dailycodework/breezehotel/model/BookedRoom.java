b46d181ee6508ec0ad23216949e06cec98a9ac7f
Kriksssss <nabibeg53@gmail.com>
2024-01-28 18:23:20 +0300

    Room, BookedRoom (v1)

--- src/main/java/com/dailycodework/breezehotel/model/BookedRoom.java
+++ src/main/java/com/dailycodework/breezehotel/model/BookedRoom.java
@@ -1,4 +1,66 @@
 package com.dailycodework.breezehotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.util.Lazy;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "check_In")
+    private Long bookingId;
+
+    @Column(name = "check_Out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_FullName")
+    private String guestFullName;
+
+    @Column(name = "guest_Email")
+    private String guestEmail;
+
+    private int numOfAdults;
+
+    @Column(name = "children")
+    private int numOfChildren;
+
+    @Column(name = "total_Guest")
+    private int totalNumOfGuest;
+
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuest() {
+        this.totalNumOfGuest = this.numOfAdults + numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+
+
 }
