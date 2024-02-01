1bfc1a90b5dcc1c583c9c19bbedc192767c47250
Kriksssss <nabibeg53@gmail.com>
2024-02-01 17:37:28 +0300

    RoomResponse, BookingResponse (v1)

---
+++ src/main/java/com/dailycodework/response/BookingResponse.java
@@ -0,0 +1,34 @@
+package com.dailycodework.response;
+
+import com.dailycodework.breezehotel.model.Room;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class BookingResponse {
+    private Long Id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestName;
+    private String guestEmail;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+    private RoomResponse room;
+
+
+    public BookingResponse(Long id, LocalDate checkInDate,
+                           LocalDate checkOutDate, String bookingConfirmationCode) {
+        Id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+}
