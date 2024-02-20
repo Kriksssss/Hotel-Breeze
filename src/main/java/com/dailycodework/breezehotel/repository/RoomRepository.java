037f695fbec9b70df32497e7ca2cf4ecc4a457db
Kriksssss <nabibeg53@gmail.com>
2024-02-20 16:13:38 +0300

    used postman added lombok and spring service annotations (v1) complement the business logic in the roomService class (v1)

--- src/main/java/com/dailycodework/breezehotel/repository/RoomRepository.java
+++ src/main/java/com/dailycodework/breezehotel/repository/RoomRepository.java
@@ -1,4 +1,7 @@
 package com.dailycodework.breezehotel.repository;
 
-public interface RoomRepository {
+import com.dailycodework.breezehotel.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
