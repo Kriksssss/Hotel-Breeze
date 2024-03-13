de01a6a71685732f8ad4dc1ebf9c13ba0e983e38
Kriksssss <nabibeg53@gmail.com>
2024-03-13 10:46:19 +0300

    BreezeHotel (v1)

--- src/main/java/com/dailycodework/breezehotel/service/IUserService.java
+++ src/main/java/com/dailycodework/breezehotel/service/IUserService.java
@@ -4,10 +4,6 @@ import com.dailycodework.breezehotel.model.User;
 
 import java.util.List;
 
-/**
- * @author Simpson Alfred
- */
-
 public interface IUserService {
     User registerUser(User user);
     List<User> getUsers();
